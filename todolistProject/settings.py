c601f17d69f969e760aa2805b48295b00150036d
tjddbs2395@soongsil.ac.kr <tjddbs2395@soongsil.ac.kr>
2025-06-26 18:43:24 +0900

    chore : cors 수정

--- todolistProject/settings.py
+++ todolistProject/settings.py
@@ -55,7 +55,11 @@ MIDDLEWARE = [
 ]
 
 #이거 맞나 추가하는거
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'todolistProject.urls'
